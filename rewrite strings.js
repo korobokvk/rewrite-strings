0e1bcd4c53ada0a39f9c17296532d0e76284dffd
Maksym Zakshevskiy <korobokvk@gmail.com>
2016-11-21 14:36:46 +0200

    Working script

---
+++ rewrite strings.js
@@ -0,0 +1,15 @@
+//This script takes a string of input file and changing it to overwrite the output file
+let fs = require('fs');
+let readline = require('readline');
+
+let filename = process.argv[2];
+readline.createInterface({
+    input: fs.createReadStream('input.txt'),
+    terminal: false
+//'line' is you basic string
+}).on('line', function(line) {
+//change your string here, like an example. The example shows a SQL request to transfer links to the home page
+   let exp = (`('${line}', 'http://somesite.domen'),\n`);
+   fs.appendFile("output.txt", exp)  
+});
+
